31905e8b1357f9e9f758803dbd2904bed69a3d42
sneha0096 <panchalsneha0096@gmail.com>
2026-02-24 23:49:42 +0530

    24th feb 2026 11:50 PM latest code is added

--- tests/login-module.spec.ts
+++ tests/login-module.spec.ts
@@ -8,46 +8,69 @@ test.use({
     }
 })
 
-test('[Login] Verify that the user cannot log in with an invalid password',
-    {
-        tag: ['@UI', '@UAT'],
+test.describe('Invalid Login Tests', {
+    tag: '@InvalidLogin',
+    annotation: {
+        type: 'Story Link',
+        description: 'Link of Story.'
+    }
+}, () => {
+    test('[Login] Verify that the user cannot log in with an invalid password',
+        {
+            tag: ['@UI', '@UAT'],
+            annotation: {
+                type: 'Test Case Link',
+                description: 'Test case 1 link here .'
+            }
+        },
+        async ({ gotoUrl, loginPage, commonUtils }) => {
+            const username = commonUtils.decryptData(process.env.USER_NAME!);
+            const password = commonUtils.decryptData(process.env.PASSWORD!);
+            await loginPage.loginOrangeHRM(username, loginModuleData.wrong_password);
+            // await expect(loginPage.errorMessage).toHaveText(loginModuleData.invalid_credentials_text);
+            await expect(loginPage.errorMessage).toHaveText("qwery");
+            await expect(loginPage.userNameInput).toBeVisible();
+        })
+
+    test('[Login] Verify that the user cannot log in with an invalid username',
+        {
+            tag: ['@UI', '@UAT'],
+            annotation: {
+                type: 'Test Case Link',
+                description: 'Test case 2 link here .'
+            }
+        }, async ({ gotoUrl, loginPage, commonUtils }) => {
+            const password = commonUtils.decryptData(process.env.PASSWORD!);
+            await loginPage.loginOrangeHRM(loginModuleData.invalid_username, password);
+            await expect(loginPage.errorMessage).toHaveText(loginModuleData.invalid_credentials_text);
+            await expect(loginPage.userNameInput).toBeVisible();
+        })
+
+    test('[Login] Verify that the user cannot log in with both an invalid username and password',
+        {
+            tag: ['@UI', '@UAT', '@DEV'],
+            annotation: {
+                type: 'Test Case Link',
+                description: 'Test case 3 link here .'
+            }
+        }, async ({ gotoUrl, loginPage, commonUtils }) => {
+            await loginPage.loginOrangeHRM(loginModuleData.invalid_username, loginModuleData.wrong_password);
+            await expect(loginPage.errorMessage).toHaveText(loginModuleData.invalid_credentials_text);
+            await expect(loginPage.userNameInput).toBeVisible();
+        })
+
+    test('[Login] Verify that the user can log in with a valid username and password', {
+        tag: ['@VISUAL', '@UAT'],
         annotation: {
             type: 'Test Case Link',
-            description: 'Test case 1 link here .'
+            description: 'Test case 4 link here .'
         }
-    },
-    async ({ gotoUrl, loginPage, commonUtils }) => {
+    }, async ({ gotoUrl, loginPage, commonUtils, leftNavigationPage }) => {
         const username = commonUtils.decryptData(process.env.USER_NAME!);
         const password = commonUtils.decryptData(process.env.PASSWORD!);
-        await loginPage.loginOrangeHRM(username, loginModuleData.wrong_password);
-        await expect(loginPage.errorMessage).toHaveText(loginModuleData.invalid_credentials_text);
-        await expect(loginPage.userNameInput).toBeVisible();
+        await loginPage.loginOrangeHRM(username, password);
+        await expect(leftNavigationPage.orangeHrmLogo).toHaveScreenshot('orange-hrm-logo.png');
+        await expect(leftNavigationPage.leftNavigationPanel).toHaveScreenshot('left-nav-panel.png');
     })
 
-test('[Login] Verify that the user cannot log in with an invalid username',
-    {
-        tag: ['@UI', '@UAT'],
-        annotation: {
-            type: 'Test Case Link',
-            description: 'Test case 2 link here .'
-        }
-    }, async ({ gotoUrl, loginPage, commonUtils }) => {
-        const password = commonUtils.decryptData(process.env.PASSWORD!);
-        await loginPage.loginOrangeHRM(loginModuleData.invalid_username, password);
-        await expect(loginPage.errorMessage).toHaveText(loginModuleData.invalid_credentials_text);
-        await expect(loginPage.userNameInput).toBeVisible();
-    })
-
-test('[Login] Verify that the user cannot log in with both an invalid username and password',
-    {
-        tag: ['@UI', '@UAT', '@DEV'],
-        annotation: {
-            type: 'Test Case Link',
-            description: 'Test case 3 link here .'
-        }
-    }, async ({ gotoUrl, loginPage, commonUtils }) => {
-        await loginPage.loginOrangeHRM(loginModuleData.invalid_username, loginModuleData.wrong_password);
-        await expect(loginPage.errorMessage).toHaveText(loginModuleData.invalid_credentials_text);
-        await expect(loginPage.userNameInput).toBeVisible();
-    })
-
+})
